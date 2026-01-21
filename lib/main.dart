cc717edc469496fb90a2b60b6bf4ff375f9029df
angelotancredi <nasix1926@gmail.com>
2026-01-21 15:34:45 +0900

    Build: Fix compilation error (missing foundation.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
